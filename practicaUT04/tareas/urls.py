5a0332558096a87d3827f301e672189943e01482
boguieboo <juliacg3@gmail.com>
2026-01-08 12:46:06 +0100

    Vista alta alumno/profesor funcionando

--- practicaUT04/tareas/urls.py
+++ practicaUT04/tareas/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('alumno/<uuid:alumno_id>/', views.detalle_alumno, name='detalle_alumno'),
     path('profesor/<uuid:profesor_id>/', views.detalle_profesor, name='detalle_profesor'),
+    path('alta_alumno/', views.alta_alumno, name='alta_alumno'),
+    path('alta_profesor/', views.alta_profesor, name='alta_profesor'),
 ]
--- practicaUT04/tareas/views.py
+++ practicaUT04/tareas/views.py
@@ -1,6 +1,9 @@
 # tareas/views.py
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 from .models import Alumno, Profesor
+from .forms import AlumnoForm, ProfesorForm
+
 
 # from django.http import HttpResponse
 # Vista para mostrar detalles de un alumno
@@ -13,4 +16,25 @@ def detalle_profesor(request, profesor_id):
     profesor = get_object_or_404(Profesor, id=profesor_id)
     return render(request, 'tareas/detalle_profesor.html', {'profesor': profesor})
 
+# Vista para dar de alta un alumno
+def alta_alumno(request):
+    if request.method == 'POST':
+        form = AlumnoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Alumno dado de alta correctamente.")
+    else:
+        form = AlumnoForm()
+    return render(request, 'tareas/alta_alumno.html', {'form': form})
+
+# Vista para dar de alta un profesor
+def alta_profesor(request):
+    if request.method == 'POST':
+        form = ProfesorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Profesor dado de alta correctamente.")
+    else:
+        form = ProfesorForm()
+    return render(request, 'tareas/alta_profesor.html', {'form': form})
 
